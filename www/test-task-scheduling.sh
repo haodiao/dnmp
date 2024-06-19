d3cf67b7b54980a230a2cb5499f6001f4ce4e416
haodiao <gxszpx@gmail.com>
2024-06-19 11:17:27 +0800

    任务demo修改

--- www/test-task-scheduling.sh
+++ www/test-task-scheduling.sh
@@ -5,12 +5,17 @@
 #
 #判断phpMyAdmin目录是否存在
 if [ ! -d /www/default/phpMyAdmin ];then
-    composer create-project phpmyadmin/phpmyadmin /www/default/phpMyAdmin
+    composer create-project phpmyadmin/phpmyadmin /www/default/phpMyAdmin \
+	&& cp /www/default/phpMyAdmin/config.sample.inc.php /www/default/phpMyAdmin/config.inc.php \
+	&& sed -i "s/localhost/mariadb/g" /www/default/phpMyAdmin/config.inc.php
 fi
 
 #判断phpRedisAdmin目录是否存在
 if [ ! -d /www/default/phpRedisAdmin ];then
-    composer create-project -s dev erik-dubbelboer/php-redis-admin /www/default/phpRedisAdmin
+    composer create-project -s dev erik-dubbelboer/php-redis-admin /www/default/phpRedisAdmin \
+	&& cp /www/default/phpRedisAdmin/includes/config.sample.inc.php /www/default/phpRedisAdmin/includes/config.inc.php \
+	&& sed -i "s/127.0.0.1/kvrocks/g" /www/default/phpRedisAdmin/includes/config.inc.php \
+	&& sed -i "s/6379/6666/g" /www/default/phpRedisAdmin/includes/config.inc.php
 fi
 
 echo "OK"
