7556b2af0c2352c21ca94655a4e1db6d57b14239
haodiao <gxszpx@gmail.com>
2024-06-20 13:06:49 +0800

    修改部分配置文件

--- www/test-crontab.sh
+++ www/test-crontab.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 #
-#安装好了之后删除此文件，
-#以及 ./app/ofelia/config.ini 对应的定时任务
 #
-#判断phpMyAdmin目录是否存在
+# 安装好了之后删除此文件，
+# 连同 ./app/ofelia/config.ini 对应的定时任务
+#
+#
+
+
+# 判断 phpMyAdmin 目录是否存在
 if [ ! -d /www/default/phpMyAdmin ];then
     composer create-project phpmyadmin/phpmyadmin /www/default/phpMyAdmin \
 	&& cp /www/default/phpMyAdmin/config.sample.inc.php /www/default/phpMyAdmin/config.inc.php \
 	&& sed -i "s/localhost/mariadb/g" /www/default/phpMyAdmin/config.inc.php
 fi
 
-#判断phpRedisAdmin目录是否存在
+
+# 判断 phpRedisAdmin 目录是否存在
 if [ ! -d /www/default/phpRedisAdmin ];then
     composer create-project -s dev erik-dubbelboer/php-redis-admin /www/default/phpRedisAdmin \
 	&& cp /www/default/phpRedisAdmin/includes/config.sample.inc.php /www/default/phpRedisAdmin/includes/config.inc.php \
